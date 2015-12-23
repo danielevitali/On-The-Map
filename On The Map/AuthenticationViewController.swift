8942f5adec9093c4012bf5e4627120d3f325efc4
Daniele Vitali <daniele.vitali89@gmail.com>
2015-12-23 17:17:32 +0100

    First commit

---
+++ On The Map/AuthenticationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Daniele Vitali on 12/23/15.
+//  Copyright © 2015 Daniele Vitali. All rights reserved.
+//
+
+import UIKit
+
+class AuthenticationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
